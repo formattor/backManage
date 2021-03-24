8a77ae9ebfe42b03c6bbe03f5a2b5918b060952b
zhangyushi <1193822368@qq.com>
2021-03-25 00:07:51 +0800

    主页完成，路由守卫token什么的

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,18 @@
 // 调用ajax接口
 $.ajaxPrefilter(function(options){
   console.log(options.url);
-  options.url='http://ajax.frontend.itheima.net'+options.url
+  options.url='http://ajax.frontend.itheima.net'+options.url;
+  // 统一有权限的请求头设置header
+  if(options.url.indexOf('/my/')!==-1){
+    options.headers={
+      Authorization:localStorage.getItem('token')||''
+    }
+  }
+  // 全局挂载complete回调
+  options.complete=function(res) {
+    if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+        localStorage.removeItem('token')
+        location.href='/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function(){
+  // 调用函数
+  getUserInfo()
+  // 将layer赋值，似乎不需要
+  // var layer=layui.layer
+  // 退出
+  $('#btnLogout').on('click',function(){
+    // console.log('ok');
+    layer.confirm('确定退出?', {icon: 3, title:'提示'}, function(index){
+      localStorage.removeItem('token')
+      location.href='/login.html'
+      layer.close(index);
+    });
+  })
+})
+
+
+// 获取用户基本信息
+function getUserInfo(){
+  $.ajax({
+    method:'GET',
+    url:'/my/userinfo',
+    // headers:{
+    //   Authorization:localStorage.getItem('token')||''
+    // },
+    success:function(res){
+      if(res.status!==0){
+        return layui.layer.msg('获取用户信息失败')
+      }
+      // 渲染图片
+      console.log(res);
+      renderAvatar(res.data)
+    },
+    // complete:function(res){
+    //   // console.log(res, 'complete')
+    //   if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+    //     localStorage.removeItem('token')
+    //     location.href='/login.html'
+    //   }
+    // }
+  })
+}
+
+// 渲染图片
+function renderAvatar(user){
+  var name=user.nickname||user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;'+ name)
+  if(user.user_pic){
+    $('.layui-nav-img').attr('src',user_pic).show()
+    $('.text_avatar').hide()
+  }else{
+    $('.layui-nav-img').hide()
+    var first=name[0].toUpperCase()
+    $('.text_avatar').html(first).show()
+  }
+}
